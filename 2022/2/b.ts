c185e07edc302f1cf2c5aa719362b2b36526f4ff
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-12-03 10:00:04 +0530

    feat: day 2, (second problem is still not done since i was busy)

---
+++ 2022/2/b.ts
@@ -0,0 +1,58 @@
+const a = await Deno.readTextFile("input.txt");
+const input = a.trim().split("\r\n");
+
+console.log("Answer:\t", solve(input));
+
+function solve(x: string[]) {
+    let my_score = 0;
+    let pos = 0;
+    while (pos < x.length) {
+        // A is rock, B is paper, C is scissors
+        // X is rock, Y is paper, Z is scissors
+
+        let move = x[pos].split(" ");
+        let round_score = calculate_initial_score(move[1]) as number;
+        if (move[1] === 'X') {
+            // Lose
+        }
+        let draw_total = calculate_draw(move[0], move[1]) as number;
+        if (did_win(move[0], move[1])) {
+            round_score += 6;
+        }
+        my_score += (round_score + draw_total);
+        pos++;
+    }
+
+    return my_score;
+}
+
+function calculate_draw(a: string, b: string) {
+    if (a == "A" && b == "X" || a == "B" && b == "Y" || a == "C" && b == "Z") {
+        return 3;
+    }
+    return 0;
+}
+
+function did_win(a: string, b: string) {
+
+    if (a == "A" && b == "Y") {
+        return true;
+    } else if (a == "B" && b == "Z") {
+        return true;
+    } else if (a == "C" && b == "X") {
+        return true;
+    }
+    return false;
+}
+
+function calculate_initial_score(x: string) {
+    if (x == "X") {
+        return 1;
+    }
+    if (x == "Y") {
+        return 2;
+    }
+    if (x == "Z") {
+        return 3;
+    }
+}
